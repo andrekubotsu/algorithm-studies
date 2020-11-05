75aa3c0335bb319b563c2ae258620149944d43cf
Andre Kubotsu <66680661+andrekubotsu@users.noreply.github.com>
2020-11-05 20:27:29 -0300

    Create intermediate.js
    First one

---
+++ intermediate.js
@@ -0,0 +1,12 @@
+
+// Intermediate Algorithm Scripting: Spinal Tap Case
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+// IMPORTANT: Regex
+
+function spinalCase(str) {
+  let newStr = str.replace(/([a-z])([A-Z])/g, "$1 $2").toLowerCase().split(/(?:_| )+/).join("-");
+  
+  return newStr;
+}
+
+spinalCase('This Is Spinal Tap');
