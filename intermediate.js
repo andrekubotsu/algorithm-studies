f2d9edaa9634a3b47cc486289c0cd0a9bc7a5418
Andre Kubotsu <66680661+andrekubotsu@users.noreply.github.com>
2020-11-06 14:52:59 -0300

    Update intermediate.js

--- intermediate.js
+++ intermediate.js
@@ -2,6 +2,7 @@
 // Intermediate Algorithm Scripting: Spinal Tap Case (from FCC)
 // Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 // IMPORTANT: Regex
+// About the solution: I really like this way, it´s simple and effective.
 
 function spinalCase(str) {
   let newStr = str.replace(/([a-z])([A-Z])/g, "$1 $2").toLowerCase().split(/(?:_| )+/).join("-");
