96e03a57d2c67be354f9949d8cc658e2e3889703
Andre Kubotsu <66680661+andrekubotsu@users.noreply.github.com>
2020-11-06 15:06:28 -0300

    Update intermediate.js

--- intermediate.js
+++ intermediate.js
@@ -1,5 +1,8 @@
 
+//=============================================================
 // Intermediate Algorithm Scripting: Spinal Tap Case (from FCC)
+//=============================================================
+
 // Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 // IMPORTANT: Regex
 // About the solution: I really like this way, it´s simple and effective.
@@ -11,3 +14,21 @@ function spinalCase(str) {
 }
 
 spinalCase('This Is Spinal Tap');
+
+//==================================
+// Intermediate Pig Latin (from FCC)
+//==================================
+
+// You need to create a program that will translate from English to Pig Latin. 
+// Pig Latin takes the first consonant (or consonant cluster) of an English word, 
+// moves it to the end of the word and suffixes an “ay”. If a word begins with a vowel 
+// you just add “way” to the end. It might not be obvious but you need to remove all the 
+// consonants up to the first vowel in case the word does not start with a vowel.
+
+function translatePigLatin(str) {
+  return str
+    .replace(/^[aeiou]\w*/, "$&way")
+    .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+}
+
+translatePigLatin("consonant");
