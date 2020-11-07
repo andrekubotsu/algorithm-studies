9bb59c0ba205c9b027b132d1fe5ab48402389ac6
Andre Kubotsu <66680661+andrekubotsu@users.noreply.github.com>
2020-11-07 16:45:22 -0300

    Update intermediate.js

--- intermediate.js
+++ intermediate.js
@@ -241,3 +241,48 @@ sumPrimes(10);
 
 
 
+//======================================================================
+// Intermediate Algorithm Scripting: Smallest Common Multiple (from FCC)
+//======================================================================
+
+const smallestCommons = arr => {
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  // Initially the solution is assigned to the highest value of the array
+  let sol = max;
+
+  for (let i = max - 1; i >= min; i--) {
+    // Each time the solution checks (i.e. sol%i===0) it won't be necessary
+    // to increment 'max' to our solution and restart the loop
+    if (sol % i) {
+      sol += max;
+      i = max;
+    }
+  }
+  return sol;
+};
+
+// test here
+smallestCommons([1, 5]);
+
+//=====================================================
+// Intermediate Algorithm Scripting: Drop it (from FCC)
+//=====================================================
+
+function dropElements(arr, func) {
+  // drop them elements.
+  var times = arr.length;
+  for (var i = 0; i < times; i++) {
+    if (func(arr[0])) {
+      break;
+    } else {
+      arr.shift();
+    }
+  }
+  return arr;
+}
+
+// test here
+dropElements([1, 2, 3, 4], function(n) {
+  return n >= 3;
+});
